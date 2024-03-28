b5eedfffb2a9f4d68023b597c8b128c9bf589ec0
lsh7465 <lsh746527@gmail.com>
2024-03-28 15:32:45 +0900

    반복문map

--- app/list/page.js
+++ app/list/page.js
@@ -1,13 +1,15 @@
-export default function RootLayout() {
+export default function List() {
+  let 상품 = ["Tomatoes", "Pasta", "Coconut"];
   return (
     <div>
-      <h2>Products</h2>
-      <div className="food">
-        <h4>상품명 $40</h4>
-      </div>
-      <div className="food">
-        <h4>상품명 $40</h4>
-      </div>
+      <h4 className="title">상품목록</h4>
+      {상품.map((a) => {
+        return (
+          <div className="food">
+            <h4>{a} $40</h4>
+          </div>
+        );
+      })}
     </div>
   );
 }
--- app/page.js
+++ app/page.js
@@ -7,11 +7,10 @@ export default function Home() {
   let link = "https://naver.com";
   return (
     <div>
-  
       {/* style 넣고 싶으면 object 자료형으로  */}
-      <h4 className="title" style={{ color: "red", fontSize: "20px" }}>
+      <h1 className="title" style={{ color: "red", fontSize: "20px" }}>
         애플프레쉬
-      </h4>
+      </h1>
       <p className="title-sub">by dev {name}</p>
       {/* html 안에 변수 넣을 땐 { } */}
       <a href={link}>링크</a>
