c300c015ed92b89342b62167e2ac5a70cb8f5de3
lsh7465 <lsh746527@gmail.com>
2024-04-02 15:20:26 +0900

    array, object state 변경하기

--- app/list/page.js
+++ app/list/page.js
@@ -3,8 +3,14 @@
 import { useState } from "react";
 
 export default function List() {
+  // const 초기상품목록 = [
+  //   { 이름: "Tomatoes", 수량: 0 },
+  //   { 이름: "Pasta", 수량: 0 },
+  //   { 이름: "Coconut", 수량: 0 },
+  // ];
+  // const [상품목록, 상품목록변경] = useState(초기상품목록);
   const 상품 = ["Tomatoes", "Pasta", "Coconut"];
-  const [수량, 수량변경] = useState(0);
+  const [수량, 수량변경] = useState([0, 0, 0]);
   return (
     <div>
       <h4 className="title">상품목록</h4>
@@ -13,17 +19,23 @@ export default function List() {
           <div className="food" key={i}>
             <img src={`/food${i}.png`} width="200" height="200" />
             <h4>{a} $40</h4>
-            <span> {수량} </span>
+            <span> {수량[i]} </span>
             <button
               onClick={() => {
-                수량변경(수량 + 1);
+                let copy = [...수량];
+                copy[i] = copy[i] + 1;
+                수량변경(copy);
               }}
             >
               +
             </button>
             <button
               onClick={() => {
-                수량변경(수량 - 1);
+                if (수량[i] > 0) {
+                  let copy = [...수량];
+                  copy[i] = copy[i] - 1;
+                  수량변경(copy);
+                }
               }}
             >
               -
