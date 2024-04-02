3bbce8b7c9a1ae2fdcca27565b26406fb5e9b17a
lsh7465 <lsh746527@gmail.com>
2024-04-02 12:11:47 +0900

    porps

--- app/cart/page.js
+++ app/cart/page.js
@@ -1,4 +1,5 @@
 export default function Cart() {
+  let 장바구니 = ["Tomatoes", "Pasta"];
   return (
     <div>
       <h4 className="title">Cart</h4>
@@ -7,11 +8,29 @@ export default function Cart() {
         <p>$40</p>
         <p>1개</p>
       </div>
-      <div className="cart-item">
-        <p>상품명</p>
-        <p>$40</p>
-        <p>1개</p>
-      </div>
+      <CartItem 상품={장바구니[0]} />
+      <CartItem 상품={장바구니[1]} />
+      {/*  <자식컴포넌트 작명={전해줄데이터} />  */}
+      <Banner content="삼성카드" />
+      <Btn color="red" />
+      <Btn color="blue" />
     </div>
   );
+}
+
+function CartItem(props) {
+  return (
+    <div className="cart-item">
+      <p>{props.상품}</p>
+      <p>$40</p>
+      <p>1개</p>
+    </div>
+  );
+}
+function Banner(props) {
+  return <h5>{props.content} 결제 행사중</h5>;
+}
+
+function Btn(props) {
+  return <button style={{ background: props.color }}>버튼</button>;
 }
